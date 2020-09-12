5eaa9a41b6b4fc72a29d94038770c6a4351170fd
tagalon <ravellarahul@gmail.com>
2020-09-11 23:53:01 -0700

    lab 3 finished!

---
+++ lab3/IntDList/BuggyIntDList.java
@@ -0,0 +1,87 @@
+/**
+ * @author Vivant Sakore on 1/29/2020
+ */
+/** @NikilSunku I don't know if we filled out the form, but we decided in the first lab that we were going to be partners for the semester **/
+public class BuggyIntDList extends IntDList {
+
+    /**
+     * @param values creates a BuggyIntDList with ints values.
+     */
+    public BuggyIntDList(Integer... values) {
+        super(values);
+    }
+
+
+    /**
+     * Merge IntDList `l` into the calling IntDList
+     * Assume that the two IntDLists being merged are sorted individually before merge.
+     * The resulting IntDList must also be sorted in ascending order.
+     *
+     * @param l Sorted IntDList to merge
+     */
+    public void mergeIntDList(IntDList l) {
+        front = sortedMerge(this.front, l.front);
+    }
+
+    /**
+     * Recursively merge nodes after value comparison
+     *
+     * @param d1 Node 1
+     * @param d2 Node 2
+     * @return Nodes arranged in ascending sorted order
+     */
+    private DNode sortedMerge(DNode d1, DNode d2) {
+
+        // FIXME: Below code has multiple problems. Debug the code to implement correct functionality.
+
+        // ------ WRITE ADDITIONAL CODE HERE AND ONLY HERE (IF NEEDED) ------
+
+        // ------------------------------------------------------------------
+        if  (d1 == null) {
+            return d2;
+        }
+        if (d2 == null) {
+            return d1;
+        }
+        if (d1.val <= d2.val) {
+            d1.next = sortedMerge(d1.next, d2);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
+            d1.next.prev = d1;
+            d1.prev = null;
+            return d1;
+        } else {
+            d2.next = sortedMerge(d1, d2.next);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
+            d2.next.prev = d2;
+            d2.prev = null;
+            return d2;
+        }
+    }
+
+
+    /**
+     * Reverses IntDList in-place (destructive). Does not create a new IntDList.
+     */
+    public void reverse() {
+
+        // FIXME: Below code has multiple problems. Debug the code to implement correct functionality.
+
+        DNode temp = null;
+        DNode p = front;
+
+        // HINT: What does this while loop do? Use Debugger and Java Visualizer to figure out.
+        BuggyIntDList L = new BuggyIntDList(12, 23, 34, 45, 56);
+        while (p != null) {
+            temp = p.prev;
+            p.prev = p.next;
+            p.next = temp;
+            p = p.prev;        // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+        }
+
+        // HINT: What does this if block do? Use Debugger and Java Visualizer to figure out.
+        if (temp != null) {
+            // ------ WRITE ADDITIONAL CODE HERE AND ONLY HERE (IF NEEDED) -----
+            back = front;
+            // -----------------------------------------------------------------
+            front = temp.prev;    // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+        }
+    }
+}
