27bf260161840996a2c140041cd647de6bdfacb0
Rahul Ravella <ravella@berkeley.edu>
2020-09-18 14:31:32 -0500

    proj1b submission

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -7,11 +7,9 @@ public class OffByN implements CharacterComparator {
     @Override
     /* Checks if the given chars are right next to each other by N */
     public boolean equalChars(char x, char y) {
-        int a = Character.toLowerCase(x);
-        int b = Character.toLowerCase(y);
 //        if (a >= 173 | b >= 173 | a <= 96 | b <= 96) {
 //            return false;
-        if (Math.abs(a - b) == N) {
+        if (Math.abs(x - y) == N) {
             return true;
         }
         return false;
