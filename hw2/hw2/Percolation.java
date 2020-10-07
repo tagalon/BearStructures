39eb571696447e3b825a871070d3544a030307cf
Rahul Ravella <ravella@berkeley.edu>
2020-10-06 23:06:40 -0500

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,94 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    public WeightedQuickUnionUF tracker;
+    public boolean[][] grid;
+    public int openSites;
+    public WeightedQuickUnionUF gridFull;
 
+    public Percolation(int N) {
+        tracker = new WeightedQuickUnionUF(N * N + 2);
+        grid = new boolean[N][N];
+        gridFull = new WeightedQuickUnionUF(N * N + 2);
+        openSites = 0;
+        for (int row = 0; row < N; row++) {
+            for(int col = 0; col < N; col++) {
+                grid[row][col] = false;
+            }
+        }
+    }
+
+    private void validate(int v1, int v2) {
+        if (v1 < 0 || v1 >= grid.length || v2 < 0 || v2 >= grid.length) {
+            throw new IndexOutOfBoundsException(
+                    v1 + ", " + v2 + " is not a valid index inside the grid where indexes are between (0, 0) and (" + (grid.length - 1) + ", " + (grid.length - 1) + ")"
+            );
+        }
+    }
+
+    private boolean indexedGrid(int row, int col) {
+        return grid[row][col];
+    }
+
+    public void open(int row, int col) {
+        validate(row, col);
+        grid[row][col] = true;
+        openSites += 1;
+        if (row == 0) {
+            tracker.union(row * grid.length + col, grid.length * grid.length);
+            gridFull.union(row * grid.length + col, grid.length * grid.length);
+        }
+        if (row == grid.length - 1) {
+            tracker.union( row * grid.length + col, grid.length * grid.length + 1);
+        }
+        for (int index = -1; index < 3; index += 2) {
+            unionRowHelperGrid(row, col, index);
+            unionColHelperGrid(row, col, index);
+        }
+    }
+
+    private void unionRowHelperGrid(int row, int col, int index) {
+        if ((row + index >= 0 && (row + index < grid.length)) && isOpen(row + index, col)) {
+            tracker.union((row + index) * grid.length + col, row * grid.length + col);
+            gridFull.union(row * grid.length + col, (row + index) * grid.length + col);
+            if (isFull(row, col) && isOpen(row + index, col)) {
+                gridFull.union((row + index) * grid.length + col, row * grid.length + col);
+            }
+        }
+    }
+
+    private void unionColHelperGrid(int row, int col, int index) {
+        if ((col + index < grid.length && col + index >= 0) && isOpen(row, col + index)) {
+            tracker.union(row * grid.length + (col + index), row * grid.length + col);
+            gridFull.union(row * grid.length + col, row * grid.length + (col + index));
+            if (isFull(row, col) && isOpen(row, col + index)) {
+                gridFull.union(row * grid.length + (col + index), row * grid.length + col);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        if ((indexedGrid(row, col))) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (isOpen(row, col) && gridFull.connected(row * grid.length + col, grid.length * grid.length)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean percolates() {
+        if (tracker.connected(grid.length * grid.length, grid.length * grid.length + 1)) {
+            return true;
+        }
+        return false;
+    }
+    public int numberOfOpenSites () {
+        return openSites;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,36 @@
 package hw2;
-
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 public class PercolationStats {
+    public double percThreshold;
+    public double[] sampleTestArray;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0)  {
+            throw new IllegalArgumentException(
+                    "T and/or N cannot be 0 or less than 0 itself"
+            );
+        }
+        sampleTestArray = new double[T];
+        for (int index = 0; index < T; index++) {
+            Percolation testGrid = pf.make(N);
+            while (!testGrid.percolates()) {
+                testGrid.open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+            sampleTestArray[index] = testGrid.openSites / Math.pow(N, 2);
+        }
+        percThreshold = mean();
+    }
+    public double mean() {
+        return StdStats.mean(sampleTestArray);
+    }
+    public double stddev() {
+        return StdStats.stddev(sampleTestArray);
+    }
+    public double confidenceLow () {
+        return percThreshold - .96 * stddev() / Math.sqrt(sampleTestArray.length);
+    }
+    public double confidenceHigh () {
+        return percThreshold + .96 * stddev() / Math.sqrt(sampleTestArray.length);
+    }
 
 }
