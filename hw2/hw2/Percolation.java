718cb8453060da2cac47aceea4d15d3489001853
Rahul Ravella <ravella@berkeley.edu>
2020-10-07 00:57:35 -0500

    fixed styling and duplicates for openSites

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -8,8 +8,9 @@ public class Percolation {
     private int openSites;
     private WeightedQuickUnionUF gridFull;
 
+    // Creates N x N grid
     public Percolation(int N) {
-        if (N <= 0) {
+        if (N <= 0)  {
             throw new IllegalArgumentException(
                     "N cannot be 0 or less than 0 itself"
             );
@@ -25,19 +26,22 @@ public class Percolation {
         }
     }
 
+    //Checks if arguments are valid reference in grid
     private void validate(int v1, int v2) {
         if (v1 < 0 || v1 >= grid.length || v2 < 0 || v2 >= grid.length) {
             throw new IndexOutOfBoundsException(v1 + ", " + v2 + " is not a valid index");
         }
     }
 
+    //Abstracted implementation for referencing grid
     private boolean indexedGrid(int row, int col) {
         return grid[row][col];
     }
 
+    //Opens a site and connects surrounding sites if need be
     public void open(int row, int col) {
         validate(row, col);
-        if (indexedGrid(row, col)) {
+        if (grid[row][col]) {
             openSites += 0;
         } else {
             openSites += 1;
@@ -56,6 +60,7 @@ public class Percolation {
         }
     }
 
+    // Unifies cells from left to right
     private void unionRowHelperGrid(int row, int col, int index) {
         if ((row + index >= 0 && (row + index < grid.length)) && isOpen(row + index, col)) {
             tracker.union((row + index) * grid.length + col, row * grid.length + col);
@@ -66,6 +71,7 @@ public class Percolation {
         }
     }
 
+    // Unifies cells from top to bottom
     private void unionColHelperGrid(int row, int col, int index) {
         if ((col + index < grid.length && col + index >= 0) && isOpen(row, col + index)) {
             tracker.union(row * grid.length + (col + index), row * grid.length + col);
@@ -76,6 +82,7 @@ public class Percolation {
         }
     }
 
+    // Checks if cell is open or not
     public boolean isOpen(int row, int col) {
         validate(row, col);
         if ((indexedGrid(row, col))) {
@@ -84,22 +91,28 @@ public class Percolation {
         return false;
     }
 
+    // Checks if cell is full or not
     public boolean isFull(int row, int col) {
-        if (isOpen(row, col) && gridFull.connected(row * grid.length + col, grid.length ^ 2)) {
+        int gridLen = grid.length;
+        if (isOpen(row, col) && gridFull.connected(row * grid.length + col, gridLen * gridLen)) {
             return true;
         }
         return false;
     }
 
+    //Checks whether the grid percolates
     public boolean percolates() {
         if (tracker.connected(grid.length * grid.length, grid.length * grid.length + 1)) {
             return true;
         }
         return false;
     }
+
+    // Returns number of open sites
     public int numberOfOpenSites() {
         return openSites;
     }
 
+    // Used to test functionality of the class
     public static void main(String[] args) { }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -3,6 +3,8 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 public class PercolationStats {
     private double[] sampleTestArray;
+
+    // Runs T experiments for each percolated grid
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0)  {
             throw new IllegalArgumentException(
@@ -18,15 +20,23 @@ public class PercolationStats {
             sampleTestArray[index] = testGrid.numberOfOpenSites() / Math.pow(N, 2);
         }
     }
+
+    // Returns mean of the sampled tests
     public double mean() {
         return StdStats.mean(sampleTestArray);
     }
+
+    // Returns std dev of sampled tests
     public double stddev() {
         return StdStats.stddev(sampleTestArray);
     }
+
+    // Returns 95% confidence interval from sampled tests
     public double confidenceLow() {
         return mean() - 1.96 * stddev() / Math.sqrt(sampleTestArray.length);
     }
+
+    // Returns 105% confidence interval from sampled tests
     public double confidenceHigh() {
         return mean() + 1.96 * stddev() / Math.sqrt(sampleTestArray.length);
     }
