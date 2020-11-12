220a674906e65d317da1d11757af348e79cb4a45
Rahul Ravella <ravella@berkeley.edu>
2020-11-11 23:45:42 -0600

    fixed structure

--- proj2d/bearmaps/proj2d/AugmentedStreetMapGraph.java
+++ proj2d/bearmaps/proj2d/AugmentedStreetMapGraph.java
@@ -65,10 +65,11 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
         TrieSet locationPrefixes = new TrieSet();
         locationPrefixes.add(prefix);
         for (Node node : listNodes) {
-            String cleanWord = cleanString(node.name());
             if (node.name() == null) {
                 continue;
-            } else if (!locationPrefixes.contains(cleanWord)){
+            }
+            String cleanWord = cleanString(node.name());
+            if (!locationPrefixes.contains(cleanWord)){
                 if (cleanWord.startsWith(prefix)) {
                     locationPrefixes.add(cleanWord);
                 }
