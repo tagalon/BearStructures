f5e5649adae574d2cf27eb29726faf449dd4920e
Rahul Ravella <ravella@berkeley.edu>
2020-12-09 12:00:14 -0600

    removed print

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -22,7 +22,6 @@ public class TestArrayDequeGold {
                     message += "addFirst("+i+")\n";
                 }
             }
-            test.printDeque();
             for (int i = 0; i < 10; i++) {
                 Integer solRemove = null;
                 Integer testRemove = null;
